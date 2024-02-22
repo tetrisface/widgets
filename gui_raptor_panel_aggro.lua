76feed0f091e1096c54ed962c5e5c4fc766c6370
autolumn <89279737+autolumn@users.noreply.github.com>
2024-02-22 20:48:02 +0100

    reduce reuse!

--- gui_raptor_panel_aggro.lua
+++ gui_raptor_panel_aggro.lua
@@ -332,7 +332,6 @@ local function CreatePanelDisplayList()
 		font:Print(I18N('ui.raptors.gracePeriod', { time = '' }), panelMarginX, PanelRow(1), panelFontSize)
 		local timeText = string.formatTime(((currentTime - gameInfo.raptorGracePeriod) * -1) - 0.5)
 		font:Print(timeText, panelMarginX + 220 - font:GetTextWidth(timeText) * panelFontSize, PanelRow(1), panelFontSize)
-		DrawPlayerAggros(stage)
 	elseif stage == stageMain then
 		local hatchEvolutionString = I18N('ui.raptors.queenAngerWithTech', { anger = gameInfo.raptorQueenAnger, techAnger = gameInfo.raptorTechAnger })
 		font:Print(hatchEvolutionString, panelMarginX, PanelRow(1), panelFontSize - Interpolate(font:GetTextWidth(hatchEvolutionString) * panelFontSize, 234, 244, 0, 0.59))
@@ -341,8 +340,6 @@ local function CreatePanelDisplayList()
 		local gain = gameInfo.RaptorQueenAngerGain_Base + gameInfo.RaptorQueenAngerGain_Aggression + gameInfo.RaptorQueenAngerGain_Eco
 		local time = string.formatTime((100 - gameInfo.raptorQueenAnger) / gain)
 		font:Print(time, panelMarginX + 200 - font:GetTextWidth(time:gsub('(.*):.*$', '%1')) * panelFontSize, PanelRow(2), panelFontSize)
-
-		DrawPlayerAggros(stage)
 
 		if #currentlyResistantToNames > 0 then
 			currentlyResistantToNames = {}
@@ -353,8 +350,6 @@ local function CreatePanelDisplayList()
 		local healthText = tostring(gameInfo.raptorQueenHealth)
 		font:Print(gameInfo.raptorQueenHealth .. '%', panelMarginX + 210 - font:GetTextWidth(healthText) * panelFontSize, PanelRow(1), panelFontSize)
 
-		DrawPlayerAggros(stage)
-
 		for i = 1, #currentlyResistantToNames do
 			if i == 1 then
 				font:Print(I18N('ui.raptors.queenResistantToList'), panelMarginX, PanelRow(11), panelFontSize)
@@ -362,6 +357,8 @@ local function CreatePanelDisplayList()
 			font:Print(currentlyResistantToNames[i], panelMarginX + 20, PanelRow(11 + i), panelFontSize)
 		end
 	end
+
+	DrawPlayerAggros(stage)
 
 	local endless = ""
 	if modOptions.raptor_endless then
